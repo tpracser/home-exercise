c7d8abf7ecc5b62c896e7ed17afe8c9e57859e48
Pracser Tamas <t.pracser@gmail.com>
2017-05-21 12:29:33 +0200

    waypoints done, remove useless css settings

--- my profile/my profile site/resources/js/script.js
+++ my profile/my profile site/resources/js/script.js
@@ -45,22 +45,10 @@ $(document).ready(function(){
     })
 
     $('.js--wp-2').waypoint(function(direction){
-        $('.js--wp-2').addClass('animated fadeInUp');
+        $('.js--wp-2').addClass('animated fadeIn');
     }, {
         offset: '50%'
     })
-
-    $('.js--wp-3').waypoint(function(direction){
-        $('.js--wp-3').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    })
-
-    $('.js--wp-4').waypoint(function(direction){
-        $('.js--wp-4').addClass('animated pulse');
-    }, {
-        offset: '50%'
-    });
 
     /* Mobile nav */
     $('.js--nav-icon').click(function(){
